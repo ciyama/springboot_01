390f9db3af5d00cfd716b9e3259c6aba99603612
Celio Iyama <celio.iyama@gmail.com>
2021-07-13 23:20:09 -0300

    Category memory basic -java Springboot

---
+++ src/main/java/com/ciyama/myfirstproject/MyfirstprojectApplication.java
@@ -0,0 +1,32 @@
+package com.ciyama.myfirstproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.ciyama.myfirstproject.entities.Category;
+import com.ciyama.myfirstproject.repositories.CategoryRepository;
+
+@SpringBootApplication
+public class MyfirstprojectApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Category cat1 = new Category(1L, "Electronics");
+		Category cat2 = new Category(2L, "Books");
+		
+		categoryRepository.save(cat1);
+		categoryRepository.save(cat2);
+		
+	}
+
+}
